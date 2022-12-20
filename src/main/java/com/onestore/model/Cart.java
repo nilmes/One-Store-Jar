867352a6d22982ca6f89a21d8c08b84524d16fee
NITESH DILIP MESHRAM <101390539+nilmes@users.noreply.github.com>
2022-12-20 23:59:10 +0530

    Update Cart.java

--- src/main/java/com/onestore/model/Cart.java
+++ src/main/java/com/onestore/model/Cart.java
@@ -52,7 +52,7 @@ public class Cart {
 	
 	
 	
-	@OneToMany(mappedBy = "cart",cascade = CascadeType.ALL)
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<ProductDto> products=new ArrayList<ProductDto>();;
 	
 
